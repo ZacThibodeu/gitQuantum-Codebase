7cac9169f29b222b895bbbfb964bc83bcbe4c751
ZacThibodeu <150251109+ZacThibodeu@users.noreply.github.com>
2023-11-24 19:18:08 -0800

    N,xt.sh

--- N,xt.sh
+++ N,xt.sh
@@ -2,23 +2,23 @@ node .bin/www
 {{@// /*** Multi line commit mention; */
 {
   {{
-    {[Any_Cores]:}: {[Processor_Min_Value]};*\".<%
+    {[Any_Cores]:}: {[Processor_Min_Value]};
   }};. }
 {
   {{
-    {[ALL_CPUs]:}: {[Processor_Max_Value]};*\".<%
+    {[ALL_CPUs]:}: {[Processor_Max_Value]};
   }};. }
 {
   {
 }
 {[Any_Cores]:}:
   {{
-    {[<%."/*writeResponse({<?php echo "nFC-Tag.{Write-Tag.Write-text:[Procesor_Min_Value]: True;}*\.js$/"?.></php?>});,.<%]:}: {[True]}; = {[Global usage Locked]:}: *{[1: Enable: True: Set;]:}
-  }}
+    {[<%."/*writeResponse({<?php echo "nFC-Tag.{Write-Tag.Write-text:[Procesor_Min_Value]: True;}*\.js$/"?.></php?>});,.<%]:}: {[True]}; = {[Global usage Locked]:}: *{[1: Enable: True: Set;]:}*\".<%]:};
+  }}; {
 }
 {[ALL_CPUs]:}:
   {{
-    {[<%."/*writeResponse({<?php echo "nFC-Tag.{Write-Tag.Write-text:[Procesor_Min_Value]: True;}*\.js$/"?.></php?>});,.<%]:}: {[True]}; = {[Global usage Locked]:}: *{[1: Enable: True: Set;]:}
-  }}
+    {[<%."/*writeResponse({<?php echo "nFC-Tag.{Write-Tag.Write-text:[Procesor_Min_Value]: True;}*\.js$/"?.></php?>});,.<%]:}: {[True]}; = {[Global usage Locked]:}: *{[1: Enable: True: Set;]:}*\".<%]:};
+  }}; {
 }
 }}
