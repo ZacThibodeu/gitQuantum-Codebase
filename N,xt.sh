eebd1bffab8821247ff6bc4d442cd8547797f463
ZacThibodeu <150251109+ZacThibodeu@users.noreply.github.com>
2023-11-24 17:14:17 -0800

    Create N,xt.sh
    Access Core Shell.

---
+++ N,xt.sh
@@ -0,0 +1,24 @@
+node .bin/www
+{{@// /*** Multi line commit mention; */
+{
+  {{
+    {[ALL_Cores]:}: {[Processor_Min_Value]};
+  }};. }
+{
+  {{
+    {[ALL_CPUs]:}: {[Processor_Max_Value]};
+  }};. }
+{
+  {
+{
+{[ALL_Cores]:}:
+  {{<%
+    writeResponse({Write-Tag.Write-text:[Procesor_Min_Value]: True;});,
+  }}<%
+}
+{[ALL_CPUs]:}:
+  {{<%
+  writeResponse({Write-Tag.Write-Text:[Processor_Max_Value]:});,
+  }}<%
+}
+}}
